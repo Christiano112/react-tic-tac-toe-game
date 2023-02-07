cdb595033cb381167c576a0a269d940ead650a89
Christiano112 <enyiachristian12@gmail.com>
2023-02-08 00:18:26 +0100

    Done with the game

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ const Square = ({value, onSquareClick}) => {
   )
 }
 
-export default function Board() {
-  const [squares, setSquares] = React.useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = React.useState(true);
+const Board = ({xIsNext, squares, onPlay}) => {
 
   const handleClick = (i) => {
     if (squares[i] || calculateWinner(squares)) return;
@@ -19,8 +17,8 @@ export default function Board() {
     const nextSquare = squares.slice();
     if (xIsNext) nextSquare[i] = "X";
     else nextSquare[i] = "O";
-    setSquares(nextSquare);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquare);
+    console.log(calculateWinner(squares));
   }
 
   const winner = calculateWinner(squares);
@@ -28,30 +26,105 @@ export default function Board() {
   if (winner) status = `Winner is ${winner}`;
   else status = `Next Player is ${xIsNext ? "X" : "O"}`;
 
+  let mySquare = [
+    {
+      id: 1,
+      index: 0
+    },
+    {
+      id: 2,
+      index: 1
+    },
+    {
+      id: 3,
+      index: 2
+    },
+    {
+      id: 4,
+      index: 3
+    },
+    {
+      id: 5,
+      index: 4
+    },
+    {
+      id: 6,
+      index: 5
+    },
+    {
+      id: 7,
+      index: 6
+    },
+    {
+      id: 8,
+      index: 7
+    },
+    {
+      id: 9,
+      index: 8
+    }
+  ]
+
   return (
     <div className='board'>
-      <h2>{status}</h2>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)}  />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)}  />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)}  />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)}  />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)}  />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)}  />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)}  />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)}  />
+      <p>{status}</p>
+      <div className="board-row" >
+      {mySquare.map((s) => (
+        <div key={s.id}>
+          <Square value={squares[s.index]} onSquareClick={() => handleClick(s.index)} />
+        </div>
+      ))}
       </div>
     </div>
   );
 }
 
+const Game = () => {
+  const [history, setHistory] = React.useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = React.useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquare = history[currentMove];
 
-function calculateWinner(squares) {
+  const handlePlay = (nextSquare) => {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquare];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  const jumpTo = (nextMove) => {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board xIsNext={xIsNext} squares={currentSquare} onPlay={handlePlay}  />
+      </div>
+      <div className='game-info'>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  )
+}
+
+export default Game
+
+const calculateWinner = (squares) => {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import Board from './App';
+import Game from './App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <Board />
+    <Game />
   </StrictMode>
 );
